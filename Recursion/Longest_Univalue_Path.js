2467fa0b3bc9f014c2d64987f0c2fa177a82028e
Bpmauge <briahanamauge@pursuit.org>
2020-07-13 12:26:41 -0400

    solve problem

--- Recursion/Longest_Univalue_Path.js
+++ Recursion/Longest_Univalue_Path.js
@@ -31,3 +31,35 @@ Output: 2
 
 Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
 */
+
+const longestUnivaluePath = function (root) {
+    let count = 0;
+
+    const findPath = (node, parent) => {
+        if (!node) {
+            return 0;
+        }
+
+        if (!node.left && !node.right) {
+            if (node.val === parent) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        let left = findPath(node.left, node.val);
+        let right = findPath(node.right, node.val);
+
+        count = Math.max(count, left + right);
+
+        if (node.val === parent) {
+            return 1 + Math.max(left, right);
+        } else {
+            return 0;
+        }
+    }
+    findPath(root);
+    return count;
+
+};
