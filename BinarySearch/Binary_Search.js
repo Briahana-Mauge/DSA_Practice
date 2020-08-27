5229a0245028d7682a098f9914f986ee327dc345
Bpmauge <briahanamauge@pursuit.org>
2020-08-27 15:01:17 -0400

    solve Binary Search iteratively

--- BinarySearch/Binary_Search.js
+++ BinarySearch/Binary_Search.js
@@ -22,3 +22,31 @@ You may assume that all elements in nums are unique.
 n will be in the range [1, 10000].
 The value of each element in nums will be in the range [-9999, 9999].
 */
+//iteratively
+const search = function (nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let index = nums[mid];
+
+        if (target === index) {
+            return mid;
+        } else if (target < index) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+
+    }
+    return -1;
+};
+
+let arr1 = [-1, 0, 3, 5, 9, 12];
+let arr2 = [-1, 0, 3, 5, 9, 12];
+let arr3 = [5];
+
+console.log(search(arr1, 9)); // 4
+console.log(search(arr2, 2)); // -1
+console.log(search(arr3, 5)); // 0
